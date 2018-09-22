976eb6d70fd6768cefdf09ee3bce4c8b74a824c8
Makan Azarshahy <oro1919@gmail.com>
2018-09-22 14:38:34 -0700

    Added framework for Submission component

---
+++ cssSelectors/app/Submission/index.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+class Submission extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      name: '',
+      address: '',
+      bio: '',
+    }
+  }
+
+  render() {
+    return (
+      <form></form>
+    );
+  }
+}
+
+export default Submission;
