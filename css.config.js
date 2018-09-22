7f1cb70a6db88144103be5add00b10f1f0c52260
Makan Azarshahy <oro1919@gmail.com>
2018-09-22 13:54:10 -0700

    Added webpack config for css selector

---
+++ css.config.js
@@ -0,0 +1,26 @@
+const path = require('path');
+
+module.exports = {
+  mode: 'development',
+  entry: path.resolve(__dirname, './cssSelectors/app/index.jsx'),
+  output: {
+    path: path.resolve(__dirname, './cssSelectors/public'),
+    filename: 'cssSelector.bundle.js',
+    publicPath: '/',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+        query: {
+          presets: ['env', 'react'],
+        }
+      }
+    ]
+  },
+  resolve: {
+    extensions: ['.js', '.jsx', '.json'],
+  }
+}
