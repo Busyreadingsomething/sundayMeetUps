435c5a113b9b9488d2e33a72fcad60d706a47419
Makan Azarshahy <oro1919@gmail.com>
2018-09-22 13:53:42 -0700

    Added App component

---
+++ cssSelectors/app/App/index.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+class App extends React.Component {
+  constructor() {
+    this.state = {
+      selected: false,
+    };
+  }
+
+  render() {
+    <h1>Hello World</h1>
+  }
+}
+
+export default App;
