037446ca428025c8a0c25e992908820b7e877f36
Makan Azarshahy <oro1919@gmail.com>
2018-09-22 15:49:42 -0700

    Implemented the rendering for input for different inputs

--- cssSelectors/app/Submission/index.jsx
+++ cssSelectors/app/Submission/index.jsx
@@ -1,18 +1,36 @@
 import React from 'react';
+import Input from '../Input/index';
 
 class Submission extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
       name: '',
-      address: '',
+      password: '',
       bio: '',
     }
   }
 
+  updateValue(key, value) {
+    this.setState(prevState => ({
+      [key]: value
+    }), () => console.log(this.state));
+  }
+
   render() {
     return (
-      <form></form>
+      <form>
+        {
+          Object.keys(this.state).map(key => (
+            <Input
+              key={key}
+              version={key}
+              value={this.state[key]}
+              update={(key, value) => this.updateValue(key, value)}
+            />)
+          )
+        }
+      </form>
     );
   }
 }
