46030693a22a2cad313bc32c4bedbc296b6cce40
Makan Azarshahy <oro1919@gmail.com>
2018-09-22 16:22:36 -0700

    Changed the props to accept data and present the data

--- cssSelectors/app/Modal/index.jsx
+++ cssSelectors/app/Modal/index.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
 
-const Modal = props => (
+const Modal = ({ data }) => (
   <div className="modal">
-    <h1>Your name is <span className="name">props.name</span></h1>
-    <h2>You live on <span className="address">props.address</span></h2>
+    <h1>Your name is: <span className="name">{data.name}</span></h1>
+    <h2>Your password is: <span className="address">{data.password}</span></h2>
     <h2>
       When I asked you about what yourself, you said:
-      <span className="bio"></span>
+      <span className="bio">{data.bio}</span>
     </h2>
   </div>
 );
